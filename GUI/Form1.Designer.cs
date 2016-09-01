1bdc502284cd28702d08e33aa2b34fcba48726cf
frc <fcaumartin@gmail.com>
2016-09-01 14:02:24 +0200

    modif taille Form1

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -87,7 +87,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 261);
+            this.ClientSize = new System.Drawing.Size(573, 261);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
